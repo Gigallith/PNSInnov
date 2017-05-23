bc1f54c93783feb45ed90133b459c889f940c6d5
Gigallith <paul.dupont@etu.unice.fr>
2017-05-23 11:11:00 +0200

    Rmi requests

--- src/main/java/fr/unice/polytech/rmi/RimRequest.java
+++ src/main/java/fr/unice/polytech/rmi/RimRequest.java
@@ -1,5 +1,14 @@
 package fr.unice.polytech.rmi;
 
+import fr.unice.polytech.transmission.answers.AddIdeaResult;
+import fr.unice.polytech.transmission.answers.JoinIdeaResult;
+import fr.unice.polytech.transmission.answers.ListIdeaResult;
+import fr.unice.polytech.transmission.answers.ListParticipantResult;
+import fr.unice.polytech.transmission.requests.AddIdea;
+import fr.unice.polytech.transmission.requests.JoinIdea;
+import fr.unice.polytech.transmission.requests.ListIdea;
+import fr.unice.polytech.transmission.requests.ListParticipant;
+
 import java.io.IOException;
 import java.rmi.*;
 /**
@@ -7,8 +16,10 @@ import java.rmi.*;
  */
 public interface RimRequest extends Remote{
 
-    public void add() throws IOException;
-    public void join() throws IOException, ClassNotFoundException;
+    AddIdeaResult add(AddIdea addIdea) throws RemoteException;
+    JoinIdeaResult join(JoinIdea joinIdea) throws RemoteException;
+    ListIdeaResult listIdea(ListIdea listIdea) throws RemoteException;
+    ListParticipantResult listParticipants(ListParticipant listParticipant) throws RemoteException;
 
 }
 
---
+++ src/main/java/fr/unice/polytech/rmi/RmiClient.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.rmi;
+
+/**
+ * Created by user on 23/05/2017.
+ */
+public class RmiClient {
+}
