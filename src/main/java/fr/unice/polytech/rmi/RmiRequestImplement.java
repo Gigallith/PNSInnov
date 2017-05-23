2723b7d67d9bbfda3b928bb8bf326f1a40a9aa78
Gigallith <paul.dupont@etu.unice.fr>
2017-05-23 11:25:23 +0200

    interface

--- src/main/java/fr/unice/polytech/rmi/RmiRequestImplement.java
+++ src/main/java/fr/unice/polytech/rmi/RmiRequestImplement.java
@@ -1,27 +1,46 @@
 package fr.unice.polytech.rmi;
 
-import java.io.IOException;
+import fr.unice.polytech.Project;
+import fr.unice.polytech.transmission.answers.*;
+import fr.unice.polytech.transmission.requests.AddIdea;
+import fr.unice.polytech.transmission.requests.JoinIdea;
+import fr.unice.polytech.transmission.requests.ListIdea;
+import fr.unice.polytech.transmission.requests.ListParticipant;
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashSet;
 
 
 /**
  * Created by user on 23/05/2017.
  */
 public class RmiRequestImplement extends UnicastRemoteObject implements RimRequest {
+    private final Project project;
 
-
-    RmiRequestImplement() throws RemoteException {
+    RmiRequestImplement(Project project) throws RemoteException {
         super();
+        this.project = project;
     }
 
     @Override
-    public void add() throws IOException {
-
+    public AddIdeaResult add(AddIdea addIdea) throws RemoteException {
+        this.project.addIdea(addIdea.getIdea());
+        return new AddIdeaResult(new RespondingCode(0));
     }
 
     @Override
-    public void join() throws IOException, ClassNotFoundException {
+    public JoinIdeaResult join(JoinIdea joinIdea) throws RemoteException {
+        this.project.addStudentToAProject(joinIdea.getIdea(),joinIdea.getStudent());
+        return new JoinIdeaResult(new RespondingCode(0));
+    }
 
+    @Override
+    public ListIdeaResult listIdea(ListIdea listIdea) throws RemoteException {
+        return new ListIdeaResult(new RespondingCode(0),new HashSet<>(project.getIdeaSet()));
+    }
+
+    @Override
+    public ListParticipantResult listParticipants(ListParticipant listParticipant) throws RemoteException {
+        return new ListParticipantResult(new RespondingCode(0), project.getStudentListOfAProject(listParticipant.getIdea()));
     }
 }
